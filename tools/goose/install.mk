5262c6152ca3cb9ee3e43e9f07b9ea4102c8d0b5
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2026-02-21 15:54:03 +0100

    Add

--- tools/goose/install.mk
+++ tools/goose/install.mk
@@ -9,7 +9,8 @@ install-macos:
 	curl -fsSL https://github.com/block/goose/releases/download/stable/download_cli.sh | CONFIGURE=false bash
 
 install-windows:
-	curl -fsSL https://github.com/block/goose/releases/download/stable/download_cli.sh | CONFIGURE=false bash
+	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/block/goose/main/download_cli.ps1" -OutFile "download_cli.ps1";
+	.\download_cli.ps1
 
 test-installation:
 	goose --version
